4840b548d77ef2f516021f7f1775306a49c26a2e
SyedHTanveer <Syedt21@gmail.com>
2021-01-25 09:21:05 -0800

    added Matrials.UI

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,23 +1,34 @@
 import '../App.css';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 
 function Main() {
   return (
     <div className="App">
       <form>
-        <h1>California House Price Prediction</h1>
-        <p>Enter the information</p>
-        <div>
+        <h1>King County House Price Prediction</h1>
+        <p>Enter the information below and click the button to see your estimation</p>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+          <Grid container justify="center" spacing={2}>
+            <Grid>
           <input
-            type="text"
-          />
+              type="text"
+            />
+            </Grid>
+            <Grid>
           <input
-            type="text"
-          />
+              type="text"
+            />
+            </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={12}>
           <Button variant="contained" color="primary">
             Check Price
           </Button>
-        </div>
+          </Grid>
+        </Grid>
       </form>
     </div>
   );
