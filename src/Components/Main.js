48daf732bb9c11b027c108f7a6d8128a23b5ec8b
SyedHTanveer <Syedt21@gmail.com>
2021-01-25 11:09:55 -0800

    added Material UI style changes

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,36 +1,121 @@
 import '../App.css';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
+import {
+  // eslint-disable-next-line no-unused-vars
+  Button, CssBaseline, TextField, FormControlLabel, Checkbox, Link, Grid, Typography, Container,
+} from '@material-ui/core';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 function Main() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <form>
-        <h1>King County House Price Prediction</h1>
-        <p>Enter the information below and click the button to see your estimation</p>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-          <Grid container justify="center" spacing={2}>
-            <Grid>
-          <input
-              type="text"
-            />
-            </Grid>
-            <Grid>
-          <input
-              type="text"
-            />
-            </Grid>
-            </Grid>
-          </Grid>
-          <Grid item xs={12}>
-          <Button variant="contained" color="primary">
-            Check Price
-          </Button>
-          </Grid>
-        </Grid>
+    <Container>
+    <CssBaseline />
+    <div className={classes.paper}>
+      <Typography component="h1" variant="h5">
+        King County House Price Prediction
+      </Typography>
+      <form className={classes.form} noValidate>
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="bedroom"
+          label="Number of Bedrooms"
+          name="bedroom"
+          autoComplete="3"
+          autoFocus
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          name="Bathroom"
+          label="Number of Bathrooms"
+          id="Bathroom"
+          autoComplete="1"
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          name="sqft_living"
+          label="House SQFT (house size)"
+          id="sqft_living"
+          autoComplete="1100"
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          name="sqft_lot"
+          label="Lot SQFT (land size)"
+          id="sqft_lot"
+          autoComplete="5650"
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          name="floor"
+          label="number of floors"
+          id="floor"
+          autoComplete="5650"
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          name="waterfront"
+          label="Is your house waterfront? (type y or n)"
+          id="waterfront"
+          autoComplete="n"
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          name="view"
+          label="do you have a view of the city?"
+          id="view"
+          autoComplete="n"
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className={classes.submit}
+        >
+          Search
+        </Button>
       </form>
     </div>
+  </Container>
   );
 }
 
